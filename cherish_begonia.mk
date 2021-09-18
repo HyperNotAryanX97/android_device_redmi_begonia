0efb8b047591f35375edb734b8e0143db50c1d35
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2021-09-18 18:59:51 +0530

    Upstreamed to POSP Trees

--- cherish_begonia.mk
+++ cherish_begonia.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 Potato Open Sauce Project
+# Copyright (C) 2019-2021 Potato Open Sauce Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -35,15 +35,11 @@ PRODUCT_NAME := cherish_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-CHERISH_BUILD_TYPE := OFFICIAL
 
-PRODUCT_GENERIC_PROPERTIES += \
-    ro.cherish.maintainer=HyperNotAryanX97
-
-BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys"
+BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="begonia-user 10 QP1A.190711.020 V12.0.7.0.QGGMIXM release-keys" \
+    PRIVATE_BUILD_DESC="begonia-user 11 RP1A.200720.011 V12.5.3.0.RGGMIXM release-keys" \
     PRODUCT_NAME="begonia" \
     TARGET_DEVICE="begonia"
 
